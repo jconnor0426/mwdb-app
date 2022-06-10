e0138fb35d48dfc07b2de5ecc24bfecfe41f390c
jconnor0426 <j.connor.mclaughlin@gmail.com>
2022-06-10 10:57:19 -0400

    Missed a dumb thing on MWDB

--- containers/mwdb/init_minio.py
+++ containers/mwdb/init_minio.py
@@ -1,10 +1,10 @@
 import minio
 import os
 
-MINIO_HOST = os.path.environ["MWDB_S3_STORAGE_ENDPOINT"]
-ACCESS_KEY = os.path.environ["MWDB_S3_STORAGE_ACCESS_KEY"]
-SECRET_KEY = os.path.environ["MWDB_S3_STORAGE_SECRET_KEY"]
-BUCKET_NAME = os.path.environ["MWDB_S3_STORAGE_BUCKET_NAME"]
+MINIO_HOST = os.environ["MWDB_S3_STORAGE_ENDPOINT"]
+ACCESS_KEY = os.environ["MWDB_S3_STORAGE_ACCESS_KEY"]
+SECRET_KEY = os.environ["MWDB_S3_STORAGE_SECRET_KEY"]
+BUCKET_NAME = os.environ["MWDB_S3_STORAGE_BUCKET_NAME"]
 
 minio.Minio(
     MINIO_HOST, access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=True
